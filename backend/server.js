f3506757e434a83b64cb90663a00d33a0472bc9b
BrandonLeone1 <brandonjleone@gmail.com>
2026-05-02 10:26:59 -0400

    Updating server to allow front-end URL now that it is deployed.

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(limiter);
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: ["https://financial-tracker-w7rv.onrender.com",
+    origin: ["https://financial-tracker-sage.vercel.app/",
         "http://localhost:5173"
     ],
     credentials: true
